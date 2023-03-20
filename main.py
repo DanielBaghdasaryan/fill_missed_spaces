613f5e59f1904894a8c519ea034dcdffb6b46324
Daniel.Baghdasaryan <daniel.baghdasaryan@agilico.ch>
2023-03-20 17:11:18 +0400

    add missing space positions to response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
+import re
 from utils import correct_text
-from typing import Dict
+from typing import Dict, List, Tuple
 
 app: Flask = Flask(__name__)
 
@@ -8,9 +9,13 @@ app: Flask = Flask(__name__)
 @app.route('/', methods=['POST'])
 def handle_post_request() -> Dict[str, str]:
     text: str = request.json["text"]
-    corrected_text: str = correct_text(text)
+    prepared_text: str = re.sub(' +', ' ', text).strip()
+    corrected_text: str
+    missed_space_positions: List[int]
+    corrected_text, missed_space_positions = correct_text(prepared_text)
     response: Dict[str, str] = {
-        "corrected_text": corrected_text
+        "corrected_text": corrected_text,
+        "missed_space_positions": missed_space_positions
     }
     return response
 
--- utils.py
+++ utils.py
@@ -1,26 +1,45 @@
 import re
 import wordninja
-from typing import List
+from typing import List, Tuple
 
-PUNCTUATIONS_PATTERN = re.compile(r'[.!?;,:]')
+PUNCTUATIONS_PATTERN: re.Pattern = re.compile(r'[.!?;,:]')
+SPACE_PATTERN: re.Pattern = re.compile(r' ')
 
 
-def correct_text(text: str) -> str:
+def correct_text(text: str) -> Tuple[str, List[int]]:
     punctuations: List[str] = []
     sub_sentences: List[str] = []
     prev_index: int = 0
+    end_index: int = 0
     for match in PUNCTUATIONS_PATTERN.finditer(text):
-        end_index: int = match.end()
+        end_index = match.end()
         punctuations.append(text[end_index - 1])
-        sub_sentences.append(text[prev_index:end_index - 1].strip())
+        sub_sentences.append(text[prev_index:end_index - 1])
         prev_index = end_index
+
+    # Case of no punctuations or no . at the end
+    if end_index < len(text):
+        sub_sentences.append(text[end_index:])
 
     corrected_sub_sentences: List[str] = [' '.join(wordninja.split(x)) for x in sub_sentences]
 
-    corrected_sentence: List[str] = []
+    corrected_sentences: List[str] = []
     for i, sentence in enumerate(corrected_sub_sentences):
         if i < len(punctuations):
-            corrected_sentence.extend([sentence, punctuations[i] + ' '])
+            corrected_sentences.extend([sentence, punctuations[i] + ' '])
         elif len(sentence) > 0:
-            corrected_sentence.append(sentence)
-    return ''.join(corrected_sentence).strip()
+            corrected_sentences.append(sentence)
+
+    corrected_text: str = ''.join(corrected_sentences)
+    return corrected_text, find_missed_space_indexes(text, corrected_text)
+
+
+def find_missed_space_indexes(original_text, corrected_text):
+    offset = 0
+    missed_indices = []
+    for match in SPACE_PATTERN.finditer(corrected_text):
+        space_index: int = match.start()
+        if original_text[space_index - offset] != ' ':
+            missed_indices.append(space_index)
+            offset += 1
+    return missed_indices
